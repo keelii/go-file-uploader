1ad64e1ff69a4a3285a2f53d19dc5ff9d7d71267
bjzhouqili <zhouqili@jd.com>
2023-12-12 11:59:08 +0800

    feat: optmz log files.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/flosch/go-humanize"
 	"github.com/flosch/pongo2/v6"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/google/uuid"
@@ -60,7 +61,7 @@ func getAccepts() string {
 func readDirs(dir string) []DirInfo {
 	dirs, err := os.ReadDir(dir)
 	if err != nil {
-		fmt.Println(err)
+		log.Error("Error reading directory:", dir)
 		return nil
 	}
 
@@ -69,7 +70,7 @@ func readDirs(dir string) []DirInfo {
 	for _, file := range dirs {
 		info, err1 := file.Info()
 		if err1 != nil {
-			fmt.Println("Error getting file info:", err1)
+			log.Error("Error getting file info:", err1)
 			return nil
 		}
 		fileInfos = append(fileInfos, info)
@@ -109,14 +110,22 @@ func readDirs(dir string) []DirInfo {
 func Render(tpl *pongo2.Template, data map[string]any) string {
 	out, err := tpl.Execute(data)
 	if err != nil {
-		fmt.Println(err)
+		log.Error(err)
 		return ""
 	}
 	return out
 }
-
+func GetLogFileMust(f string) *os.File {
+	logFile, e1 := os.OpenFile(f, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if e1 != nil {
+		panic(e1)
+	}
+	return logFile
+}
 func main() {
 	addr := flag.String("addr", ":3000", "HTTP服务地址")
+	appLogFile := flag.String("appLogFile", "./app.log", "应用日志文件")
+	reqLogFile := flag.String("reqLogFile", "./request.log", "请求日志文件")
 	uploadPath := flag.String("uploadPath", "/tmp/uploads", "上传文件保存路径")
 	maxFileSize := flag.Int64("maxFileSize", 2*1024*1024, "上传文件大小限制")
 	urlPrefix := flag.String("urlPrefix", "http://localhost/static", "上传文件访问前缀")
@@ -125,14 +134,22 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("--------------------------------------------------")
-	fmt.Println("        addr:", *prd)
-	fmt.Println("  uploadPath:", *uploadPath)
-	fmt.Println(" maxFileSize:", *maxFileSize)
-	fmt.Println("   urlPrefix:", *urlPrefix)
-	fmt.Println("    rootPass:", (*rootPass)[:3]+"***")
-	fmt.Println("         prd:", *prd)
-	fmt.Println("--------------------------------------------------")
+	requestLogOutput := os.Stdout
+	if *prd {
+		log.SetOutput(GetLogFileMust(*appLogFile))
+		requestLogOutput = GetLogFileMust(*reqLogFile)
+	}
+
+	log.Info("--------------------------------------------------")
+	log.Info("        addr: ", *addr)
+	log.Info("  reqLogFile: ", *reqLogFile)
+	log.Info("  appLogFile: ", *appLogFile)
+	log.Info("  uploadPath: ", *uploadPath)
+	log.Info(" maxFileSize: ", *maxFileSize)
+	log.Info("   urlPrefix: ", *urlPrefix)
+	log.Info("    rootPass: ", (*rootPass)[:3]+"***")
+	log.Info("         prd: ", *prd)
+	log.Info("--------------------------------------------------")
 
 	if *rootPass == "" {
 		panic("-rootPass is required")
@@ -144,17 +161,18 @@ func main() {
 	app := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
 	})
-
 	app.Use(logger.New(logger.Config{
 		Format: "${time} ${ip}:${port} ${status} - ${user} ${method} ${path}\n",
 		CustomTags: map[string]logger.LogFunc{
 			"time": func(output logger.Buffer, c *fiber.Ctx, data *logger.Data, extraParam string) (int, error) {
-				return output.WriteString(time.Now().Format("2006-01-02 15:04:05.000"))
+				return output.WriteString(time.Now().Format("2006/01/02 15:04:05.000000"))
 			},
 			"user": func(output logger.Buffer, c *fiber.Ctx, data *logger.Data, extraParam string) (int, error) {
 				return output.WriteString(c.Locals("username").(string))
 			},
 		},
+		Output:        requestLogOutput,
+		DisableColors: *prd,
 	}))
 	app.Use(basicauth.New(basicauth.Config{
 		Users: map[string]string{
@@ -168,29 +186,6 @@ func main() {
 
 	tpl, _ := pongo2.FromString(indexView)
 
-	app.Get("/delete", func(c *fiber.Ctx) error {
-		dir := c.Query("dir", "")
-		file := c.Query("file", "")
-
-		if dir != "" {
-			targetDir := fmt.Sprintf("%s/%s", *uploadPath, dir)
-			if file == "" {
-				_ = os.RemoveAll(targetDir)
-				fmt.Println("rm_a", dir)
-			} else {
-				targetFile := fmt.Sprintf("%s/%s/%s", *uploadPath, dir, file)
-
-				_ = os.Remove(targetFile)
-				fmt.Println("rm_f", dir, file)
-
-				if par, err := os.ReadDir(targetDir); err == nil && len(par) == 0 {
-					_ = os.Remove(targetDir)
-					fmt.Println("rm_d", dir)
-				}
-			}
-		}
-		return c.Redirect("/", fiber.StatusTemporaryRedirect)
-	})
 	app.Get("/", func(c *fiber.Ctx) error {
 		data := fiber.Map{
 			"url_prefix": urlPrefix,
@@ -209,6 +204,32 @@ func main() {
 
 		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
 		return c.SendString(Render(tpl, data))
+	})
+	app.Get("/req_log", func(c *fiber.Ctx) error {
+		return c.SendStream(GetLogFileMust(*reqLogFile))
+	})
+	app.Get("/delete", func(c *fiber.Ctx) error {
+		dir := c.Query("dir", "")
+		file := c.Query("file", "")
+
+		if dir != "" {
+			targetDir := fmt.Sprintf("%s/%s", *uploadPath, dir)
+			if file == "" {
+				_ = os.RemoveAll(targetDir)
+				log.Warn("rm_a: ", dir)
+			} else {
+				targetFile := fmt.Sprintf("%s/%s/%s", *uploadPath, dir, file)
+
+				_ = os.Remove(targetFile)
+				log.Warn("rm_f: ", dir, file)
+
+				if par, err := os.ReadDir(targetDir); err == nil && len(par) == 0 {
+					_ = os.Remove(targetDir)
+					log.Warn("rm_d: ", dir)
+				}
+			}
+		}
+		return c.Redirect("/", fiber.StatusTemporaryRedirect)
 	})
 	app.Post("/", func(c *fiber.Ctx) error {
 		data := fiber.Map{
@@ -288,5 +309,6 @@ func main() {
 		return c.Redirect("/", fiber.StatusSeeOther)
 	})
 
+	log.Infof("Server started at http://localhost%s\n", *addr)
 	app.Listen(*addr)
 }
